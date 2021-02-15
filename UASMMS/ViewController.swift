d4611ad403b5f9e31a2596eaac4b61207b0e9c4d
Matheus Arnold Richardo <matheusarnoldrichardo@gmail.com>
2021-02-15 05:57:39 -0800

    3rd Update

--- UASMMS/ViewController.swift
+++ UASMMS/ViewController.swift
@@ -7,15 +7,22 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var searchBar: UITextField!
-
+    @IBOutlet weak var errorLabel: UILabel!
+    @IBOutlet var table: UITableView!
+    
+    var words = [Word]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
+        table.delegate = self
+        table.dataSource = self
         self.searchBar.delegate = self
+        
+        searchBar.addTarget(self, action: #selector(checkAndDisplayError(textfield:)), for: .editingChanged)
+        // Do any additional setup after loading the view.
     }
 
     //Hide Keyboard
@@ -25,8 +32,42 @@ class ViewController: UIViewController, UITextFieldDelegate {
 
     //Return Key
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        searchBar.resignFirstResponder()
+        searchWord()
         return (true)
+    }
+    
+    func searchWord(){
+        searchBar.resignFirstResponder()
+        
+        guard let text = searchBar.text, !text.isEmpty else {
+            return
+        }
+        
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return words.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    //func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    //    tableView.deselectRow(at: indexPath, animated: true)
+    //}
+    
+    @objc func checkAndDisplayError (textfield: UITextField) {
+        if (textfield.text?.count ?? 0 < 3) {
+            errorLabel.text = "Please enter more than 2 characters"
+        }
+        else {
+            errorLabel.text = ""
+        }
     }
 }
 
+struct Word {
+    
+}
